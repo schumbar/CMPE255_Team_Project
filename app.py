0554043440a359c864171f3ad4845fa9685b682e
Shawn Chumbar <schumbar@ucsc.edu>
2023-12-15 22:23:08 -0800

    Checking to see if port binding helps

--- app.py
+++ app.py
@@ -73,4 +73,7 @@ def generate_summary():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    generate_summary()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    # generate_summary()
